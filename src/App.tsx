d38e5ba0029a4a75b4ad5ecc38a28a24aaa150ea
Samoth112 <tc.thomascho@gmail.com>
2021-07-18 12:45:52 -0400

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import './App.css';
 
 function App(): React.ReactElement {
   return (
-    <div className="main-grid">
-      <section className="hero main-grid__sub-grid">
-        <img className="img img--centered" src="https://i.imgur.com/HVih4Qp.png"></img>
-        {/* make this a router to hold the static, edit, and error views */}
-        <section className="overlay">
+    <Router>
+      <Route exact path="/articles/:id">
+        <div className="main-grid">
+          <section className="hero main-grid__sub-grid">
+            <img className="img img--centered" src="https://i.imgur.com/HVih4Qp.png"></img>
+            {/* make this a router to hold the static, edit, and error views */}
+            <section className="overlay">
+              <Router>
+                <Route exact path="/articles/:id">
 
-        </section>
-      </section>
-      <section className="article">
+                </Route>
+                <Route path="/articles/:id/edit">
 
-      </section>
-    </div>
+                </Route>
+              </Router>
+            </section>
+          </section>
+          <section className="article">
+
+          </section>
+        </div>
+      </Route>
+    </Router>
   );
 }
 
