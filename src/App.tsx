f23121673f25be8a2db04e9dff962fdf5741892d
Samoth112 <tc.thomascho@gmail.com>
2021-07-18 12:17:48 -0400

    created grid structure for hero image

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import './App.css';
 function App(): React.ReactElement {
   return (
     <div className="main-grid">
+      <section className="hero main-grid__sub-grid">
+        <img className="img img--centered" src="https://i.imgur.com/HVih4Qp.png"></img>
+        <section className="overlay">
 
+        </section>
+      </section>
+      <section className="article">
 
+      </section>
     </div>
   );
 }
