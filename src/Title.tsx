9e7c358da83f1984d60fff5283a1f79c5df8bd08
Samoth112 <tc.thomascho@gmail.com>
2021-07-19 23:09:44 -0400

    change title to persistedTitle for consistency

--- src/Title.tsx
+++ src/Title.tsx
@@ -2,18 +2,18 @@ import React, { useEffect } from 'react';
 import {loadState} from './localStorage';
 
 export default function Title(): React.ReactElement {
-  const title = loadState().pageTitle;
-
+  const persistedTitle = loadState().pageTitle;
+  
   useEffect(() => {
-    if(title !== document.title) {
-      document.title = title;
+    if(persistedTitle !== document.title) {
+      document.title = persistedTitle;
     }
   });
 
   return(
     <p className="title">
       {/* 400 is the heaviest weight available */}
-      {title}
+      {persistedTitle}
     </p>
   )
 }
